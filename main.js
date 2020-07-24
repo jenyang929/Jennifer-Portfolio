a6a9786d2990151a61d49913e7c054de91329236
Jennifer Yang <jenyang929@gmail.com>
2020-07-23 21:17:52 -0400

    Feat: finished adding 2 projects

--- main.js
+++ main.js
@@ -10,18 +10,73 @@ mainNavlinks.forEach(link => {
       behavior: "smooth",
       block: "start"
     })
-
   })
 })
 
 
+const textWrapper = document.querySelectorAll('.ml12')[0];
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml12 .letter',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 2300,
+    delay: (el, i) => 500 + 30 * i
+  })
+
+
+  anime.timeline({loop: false})
+  .add({
+    targets: '.ml5 .line',
+    opacity: [0.5,1],
+    scaleX: [0, 1],
+    easing: "easeInOutExpo",
+    duration: 1000
+  })
+  .add({
+    targets: '.ml5 .line',
+    duration: 1200,
+    easing: "easeOutExpo",
+    translateY: (el, i) => (-0.625 + 0.625*2*i) + "em"
+  }).add({
+    targets: '.ml5 .ampersand',
+    opacity: [0,1],
+    scaleY: [0.5, 1],
+    easing: "easeOutExpo",
+    duration: 1000,
+    offset: '-=600'
+  }).add({
+    targets: '.ml5 .letters-left',
+    opacity: [0,1],
+    translateX: ["0.5em", 0],
+    easing: "easeOutExpo",
+    duration: 1200,
+    offset: '-=300'
+  }).add({
+    targets: '.ml5 .letters-right',
+    opacity: [0,1],
+    translateX: ["-0.5em", 0],
+    easing: "easeOutExpo",
+    duration: 1200,
+    offset: '-=600'
+  })
+
+
+
+
+// RESEARCH POSTER
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
 
 openModalButtons.forEach(button => {
   button.addEventListener('click', () => {
+
     const modal = document.querySelector(button.dataset.modalTarget)
     openModal(modal)
   })
@@ -52,3 +107,6 @@ overlay.addEventListener('click', () => {
     closeModal(modal)
   })
 })
+
+
+
